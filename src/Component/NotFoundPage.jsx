46b1b96945005a86f8d3ef929b3b72486055e5d6
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2024-01-08 22:35:20 +0530

    Update NotFoundPage.jsx

--- src/Component/NotFoundPage.jsx
+++ src/Component/NotFoundPage.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function NotFoundPage() {
   return (
     <div className='pn-cont'>
-        <Link to='/home' className='text-decoration-none mb-4'>
+        <Link to='/music/home' className='text-decoration-none mb-4'>
               <button 
               className='back-btn'>
                 <i className='fa fa-arrow-left'></i>
